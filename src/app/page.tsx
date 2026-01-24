06d3486dfc16d9fd181f5dbd9ad1d1d841facc5c
Danielle Glazier <danielleglazier@yahoo.com>
2026-01-23 23:39:38 -0600

    Restore FamilyTreeLanding component now that Tailwind is working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import './globals.css';
+import FamilyTreeLanding from '@/content/hero/FamilyTreeLanding';
 
 export default function HomePage() {
-  return (
-    <div className="min-h-screen bg-red-500 flex items-center justify-center">
-      <div className="text-center text-white">
-        <h1 className="text-4xl font-bold mb-4">Testing Tailwind Classes</h1>
-        <p className="text-xl">If you see RED background, Tailwind works!</p>
-      </div>
-    </div>
-  );
+  return <FamilyTreeLanding />;
 }

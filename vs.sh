18e0d154fae343c25f34dc70da744cfa472fec22
Danielle Glazier <danielleglazier@yahoo.com>
2026-01-23 22:56:39 -0600

    Update dev logs and scripts

--- vs.sh
+++ vs.sh
@@ -1,1 +1,3 @@
-cat /Users/danid/ask-beau-tox/prisma/schema.prisma
+cat /Users/danid/ask-beau-tox/prisma/schema.prismagit add .
+git commit -m "fix: simplify vault page and resolve TypeScript errors"
+git push
